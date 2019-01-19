8d3403e4c7f540af57ab4ef8f4186da455faea49
Evandro Junior <evandrojr@gmail.com>
2019-01-19 07:46:53 -0300

    compilando!

--- lib/main.dart
+++ lib/main.dart
@@ -6,44 +6,46 @@ void main() => runApp(new MaterialApp(home: new MyApp()));
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
-  
 }
 
-class _MyAppState extends State<MyApp>{
-
+class _MyAppState extends State<MyApp> {
   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
     var android = new AndroidInitializationSettings('app_icon');
     var iOS = new IOSInitializationSettings();
     var initializationSettings = InitializationSettings(android, iOS);
-    flutterLocalNotificationsPlugin.initialize(initializationSettings, selectNotification: onSelectNotification);
-
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: onSelectNotification);
   }
 
-  Future onSelectNotification(String payload){
+  Future onSelectNotification(String payload) {
     debugPrint("payload: $payload");
-    showDialog(context: context, builder: (_) => new AlertDialog(
-      title: new Text('Notification'),
-      content: new Text('$payload'),
-    );
+    showDialog(
+        context: context,
+        builder: (_) => new AlertDialog(
+              title: new Text('Notification'),
+              content: new Text('$payload'),
+            ));
   }
 
   @override
-  Widget build  (BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(
         title: new Text('O Mais Importante'),
       ),
-      body: new Center(
-        child: new Text(
-          'Missão do dia: ', 
-          style: Theme.of(context).textTheme.headline,
-        ),
-      ),
+      body: new Center(child: new RaisedButton(onPressed: showNotification)),
     );
+  }
+
+  showNotification() async {
+    var android = AndroidNotificationDetails('channelId', 'channelName', 'channelDescription');
+    var iOS = new IOSNotificationDetails();
+    var platform = NotificationDetails(android, iOS);
+    await flutterLocalNotificationsPlugin.show( 3, 'Attention', 'Two new messages', platform);
   }
 }
